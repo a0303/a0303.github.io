279f889bfddd4d27fb1be96d6b298f65bfc46693
a0303 <34296881+a0303@users.noreply.github.com>
2018-01-24 05:29:41 +0900

    a

---
+++ timer3.js
@@ -0,0 +1,62 @@
+var PassSec;   // 秒数カウント用変数
+AFRAME.registerComponent('set-image', {
+  schema: {
+    on: {type: 'string'},
+    target: {type: 'selector'},
+    src: {type: 'string'},
+    dur: {type: 'number', default: 300}
+  },
+
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+
+    this.startShowing();
+
+    el.addEventListener(data.on, function () {
+      // Fade out image.
+      data.target.emit('set-image-fade');
+      // Wait for fade to complete.
+      setTimeout(function () {
+        // Set image.
+        data.target.setAttribute('material', 'src', data.src);
+      }, data.dur);
+    });
+  },
+
+  /**
+   * Setup fade-in + fade-out.
+   */
+  startShowing: function () {
+    var data = this.data;
+    var targetEl = this.data.target;
+    PassSec = 0;
+
+    // Only set up once.
+    if (targetEl.dataset.setImageFadeSetup) { 
+      //PassageID = setInterval('showPassage()',1000);
+      return; }
+    targetEl.dataset.setImageFadeSetup = true;
+
+    PassageID = setInterval('showPassage()',1000);
+    
+    // Create animation.
+    targetEl.setAttribute('animation__fade', {
+      property: 'material.color',
+      startEvents: 'set-image-fade',
+      dir: 'alternate',
+      dur: data.dur,
+      from: '#FFF',
+      to: '#000'
+    });
+  }
+});
+function showPassage() {
+   PassSec++;   // カウントアップ
+   var msg = "ボタンを押してから " + PassSec + "秒が経過しました。";   // 表示文作成
+   document.getElementById("PassageArea").innerHTML = msg;   // 表示更新
+}
+function stopShowing() {
+   clearInterval( PassageID );   // タイマーのクリア
+   document.getElementById("startcount").disabled = false;   // 開始ボタンの有効化
+}
