2bd0bf49ba2d915b47916bef126850dbac8e160b
a0303 <34296881+a0303@users.noreply.github.com>
2018-01-24 04:12:13 +0900

    a

--- timer2.js
+++ timer2.js
@@ -8,10 +8,21 @@ AFRAME.registerComponent('set-image', {
   },
 
   init: function () {
-    PassSec = 0;   // カウンタのリセット
-   PassageID = setInterval('showPassage()',1000);   // タイマーをセット(1000ms間隔)
-   document.getElementById("startcount").disabled = true;   // 開始ボタンの無効化
+        var data = this.data;
+    var el = this.el;
+
+    this.setupFadeAnimation();
+
+    el.addEventListener(data.on, function () {
+
+    });
   },
+
+  startShowing: function () {
+    PassSec = 0;   // カウンタのリセット
+    PassageID = setInterval('showPassage()',1000);   // タイマーをセット(1000ms間隔)
+    document.getElementById("startcount").disabled = true;   // 開始ボタンの無効化
+  }
 
   /**
    * Setup fade-in + fade-out.
@@ -26,4 +37,10 @@ function showPassage() {
 function stopShowing() {
    clearInterval( PassageID );   // タイマーのクリア
    document.getElementById("startcount").disabled = false;   // 開始ボタンの有効化
+}
+// 繰り返し処理の開始
+function startShowing() {
+   PassSec = 0;   // カウンタのリセット
+   PassageID = setInterval('showPassage()',1000);   // タイマーをセット(1000ms間隔)
+   document.getElementById("startcount").disabled = true;   // 開始ボタンの無効化
 }
