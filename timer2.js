1ab85f886da98ba96996d39ef30d0254f604c33a
a0303 <34296881+a0303@users.noreply.github.com>
2018-01-24 03:28:33 +0900

    a

---
+++ timer2.js
@@ -0,0 +1,29 @@
+var PassSec;   // 秒数カウント用変数
+AFRAME.registerComponent('set-image', {
+  schema: {
+    on: {type: 'string'},
+    target: {type: 'selector'},
+    src: {type: 'string'},
+    dur: {type: 'number', default: 300}
+  },
+
+  init: function () {
+    PassSec = 0;   // カウンタのリセット
+   PassageID = setInterval('showPassage()',1000);   // タイマーをセット(1000ms間隔)
+   document.getElementById("startcount").disabled = true;   // 開始ボタンの無効化
+  },
+
+  /**
+   * Setup fade-in + fade-out.
+   */
+   
+});
+function showPassage() {
+   PassSec++;   // カウントアップ
+   var msg = "ボタンを押してから " + PassSec + "秒が経過しました。";   // 表示文作成
+   document.getElementById("PassageArea").innerHTML = msg;   // 表示更新
+}
+function stopShowing() {
+   clearInterval( PassageID );   // タイマーのクリア
+   document.getElementById("startcount").disabled = false;   // 開始ボタンの有効化
+}
